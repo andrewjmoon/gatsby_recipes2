2f4112887008fc92609caaa1fb95959896760f6c
Andrew Moon <andrewjm80@gmail.com>
2023-03-09 14:10:09 -0600

    commit4

---
+++ functions/validate-user.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch")
+
+exports.handler = async (event, context) => {
+    const {user} = context.clientContext
+    if(user){
+        const userID = user.sub
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                'X-Hasura-User-Id': userID,
+                'X-Hasura-Role': `${user.app_metadata.role[0]}`
+            })
+        }
+    }
+return {
+    statusCode: 200,
+    body: JSON.stringify({
+        'X-Hasura-Role': 'anonymous'
+        })
+    }
+}
